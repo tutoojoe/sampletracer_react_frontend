ec59688bd53cf3dfac4890fca2968672ece5d3b0
Tutoo Joe Philip <tutoojoephilip@gmail.com>
2022-05-09 12:58:01 +0530

    url updated-priductdetail, process & accessories

--- src/components/requestAPIs.jsx
+++ src/components/requestAPIs.jsx
@@ -5,18 +5,22 @@ const requestAPIs = {
   login: `api/user/login/`,
   logout: `api/user/logout/`,
   products: `api/products/`,
+  productDetail: `api/products/${id}/`,
   addProduct: `api/products/add_product/`,
   productgroups: `api/products/product_groups/`,
   measurements: `api/products/measurements/`,
   colors: `api/products/colors/`,
-  colordetail: `api/products/colors/${id}`,
+  colorDetail: `api/products/colors/${id}/`,
   stylecombo: `api/products/stylecombo/`,
-  stylecombodetail: `api/products/stylecombo/${id}`,
+  stylecomboDetail: `api/products/stylecombo/${id}/`,
   seasons: `api/products/seasons/`,
-  seasondetail: `api/products/seasons/${id}`,
-
+  seasonDetail: `api/products/seasons/${id}/`,
+  processes: `api/products/processes/`,
+  processDetail: `api/products/${id}/`,
+  accessories: `api/products/accessories/`,
+  accessoriesDetail: `api/products/accessories/${id}/`,
   suppliers: `api/suppliers/`,
-  supplierdetail: `api/suppliers/${id}`,
+  supplierDetail: `api/suppliers/${id}/`,
   token: `api/token/`,
   tokenverify: `api/token/verify/`,
 
@@ -24,11 +28,11 @@ const requestAPIs = {
 
   dashboard: `api/dashboard/`,
   users: `api/users/`,
-  userdetail: `api/user/${id}`,
+  userDetail: `api/user/${id}/`,
   customers: `api/customers/`,
-  customerdetail: `api/customers/${id}`,
+  customerDetail: `api/customers/${id}/`,
   merchandisers: `api/merchandisers/`,
-  merchandiserDetail: `api/merchandiser/${id}`,
+  merchandiserDetail: `api/merchandiser/${id}/`,
 };
 
 export default requestAPIs;
